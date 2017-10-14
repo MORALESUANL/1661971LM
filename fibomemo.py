0ef90ba7d2328b58978ee4df8d5a76893f8dd4a5
MORALESUANL <30931301+MORALESUANL@users.noreply.github.com>
2017-10-14 12:41:24 -0500

    EVA4.RECURSIVO CON MEMORIA

---
+++ fibomemo.py
@@ -0,0 +1,19 @@
+global contador
+contador = 0
+cache = {0: 0, 1: 1}
+
+def fib(m):
+	global contador
+	contador = contador +1
+	if m not in cache:
+		cache[m] = fib(m-1) + fib(m-2)
+	return cache[m]
+
+y=[]
+e=[]
+for i in rango(1,100):
+	y.append(i)
+	contador=0
+	cache = {0: 0, 1: 1}
+	fib(i)
+	e.append(contador)
